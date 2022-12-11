b233752afcccef1a5727f86b0912cfa60b349cb7
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-12-11 18:44:47 -0300

    feat: get word value on letter change

--- src/components/Words/Word/Letter.tsx
+++ src/components/Words/Word/Letter.tsx
@@ -1,7 +1,17 @@
-export function Letter() {
+type LetterProps = {
+  index: number
+  handleLetterChange: (value: any, index: number) => void
+}
+
+export function Letter({ index, handleLetterChange }: LetterProps) {
+  const handleChange = (event: any) => {
+    const { value } = event.target;
+    handleLetterChange(value, index);
+  };
+
   return (
     <div className="m-1">
-      <input maxLength={1} className="text-center uppercase text-5xl w-20 h-20 border-4 border-brand-500 rounded-md text-white"/>
+      <input id="letter" autoComplete="off" onChange={handleChange} maxLength={1} className="text-center uppercase text-5xl w-20 h-20 border-4 border-brand-500 rounded-md text-white"/>
     </div>
   )
 }
--- src/components/Words/Word/index.tsx
+++ src/components/Words/Word/index.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { Letter } from "./Letter";
 
+const WORD_SIZE = 5;
+
 export function Word() {
+  const [data] = useState([...Array(WORD_SIZE).keys()]);
+
+  const word = ['', '', '', '', '']
+
+  function handleLetterChange(value: string, index: number) {
+    word[index] = value;
+    const wordIsComplete = word.every((letter) => letter !== '');
+
+    console.log('word: ', word);
+    console.log('wordIsComplete: ', wordIsComplete);
+  }
+
   return (
     <div className="flex">
-      <Letter/>
-      <Letter/>
-      <Letter/>
-      <Letter/>
-      <Letter/>
+      {data.map((data, index) => (
+        <div key={index}>
+          <Letter index={index} handleLetterChange={handleLetterChange} />
+        </div>
+      ))}
     </div>
   )
 }
