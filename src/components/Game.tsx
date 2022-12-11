688a4bb2eed1a82f74c77bbeaad097ee603c2939
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-12-11 19:17:59 -0300

    refactor: change title

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,7 +3,7 @@ import { Words } from "./Words";
 export function Game() {
   return (
     <>
-      <h1 className="text-center text-5xl pt-10 mb-20">Tremoo</h1>
+      <h1 className="text-center text-5xl pt-10 mb-20">Guess the word</h1>
       <div className="flex justify-center"><Words/></div>
     </>
   )
