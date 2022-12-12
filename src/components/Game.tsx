c1c3383622884f0a51542f8637d70573baae1adc
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-12-11 21:32:53 -0300

    chore: add TODOs

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,13 @@
 import { Words } from "./Words";
 
+// TODO mudar focus automaticamente ao digitar a letra
+// TODO permitir a navegação entre as letras da palavra pelas setas do teclado
+// TODO travar o jogo quando o usuário acertar a palavra
+// TODO mostrar a palavra correta quando o usuário errar
+// TODO implementar o teclado virtual
+// TODO gerar a palavra aleatoriamente
+// TODO permitir apenas palavras reais e exibir mensagem de erro caso digite uma palavra inválida
+
 export function Game() {
   return (
     <>
